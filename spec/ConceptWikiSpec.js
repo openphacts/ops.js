9cf981051b3cf83b3db2dcd7ce88f784d56a8ade
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2013-04-18 14:12:59 +0100

    namespace for cw code

--- spec/ConceptWikiSpec.js
+++ spec/ConceptWikiSpec.js
@@ -2,7 +2,7 @@ describe("Concept Wiki", function() {
   var searcher, appID, appKey;
 
   beforeEach(function() {
-    searcher = new ConceptWikiSearch("https://ops2.few.vu.nl/search/");
+    searcher = new Ops.ConceptWikiSearch("https://ops2.few.vu.nl/search/");
     appID = $.url().param('app_id');
     appKey = $.url().param('app_key');
   });
@@ -15,7 +15,7 @@ describe("Concept Wiki", function() {
       expect(searcher.byTag).toHaveBeenCalled();
     });
     it("and return a response", function() {
-      var callback=function(response){
+      var callback=function(success, status, response){
         var result = searcher.parseResponse(response);
         expect(result[0]).toBeDefined();
       };
@@ -30,6 +30,12 @@ describe("Concept Wiki", function() {
       runs(function() {
           expect(callback).toHaveBeenCalled();
       });
+    });
+    it("and handle errors", function() {
+      var callback=function(success, status){
+        expect(success).toEqual(false);
+      };
+      searcher.byTag(appID,appKey, 'Aspirin', '20', '4', '07a84994-e464-4ba-a4b96fa3d197', callback);
     });
   });
 });
--- src/ConceptWikiSearch.js
+++ src/ConceptWikiSearch.js
@@ -1,12 +1,12 @@
-function ConceptWikiSearch(baseURL) {
+Ops.ConceptWikiSearch = function (baseURL) {
     this.baseURL = baseURL;
 }
 
-ConceptWikiSearch.prototype.byTag = function(appID, appKey, query, limit, branch, type, callback) {
-    var conceptWikiSearcher = $.ajax({
-        dataType: "jsonp",
-        url: this.baseURL + "byTag?_callback=?",
+Ops.ConceptWikiSearch.prototype.byTag = function(appID, appKey, query, limit, branch, type, callback) {
+    var conceptWikiSearcher = $.jsonp({
+        url: this.baseURL + "byTag",
         cache: true,
+        callbackParameter: "_callback",
         data: {
             q: query,
             limit: limit,
@@ -14,14 +14,18 @@ ConceptWikiSearch.prototype.byTag = function(appID, appKey, query, limit, branch
             uuid: type,
             app_id: appID,
             app_key: appKey
+        },
+        success: function(response, status, request) {
+            callback.call(this, true, 200, response.result.primaryTopic.result);
+        },
+        // no status codes due to the nature of jsonp, just a failure message
+        error: function(options, status) {
+            callback.call(this, false);
         }
-    });
-    conceptWikiSearcher.success(function (response) {
-        callback.call(this, response.result.primaryTopic.result);
     });
 }
 
-ConceptWikiSearch.prototype.parseResponse = function(response) {
+Ops.ConceptWikiSearch.prototype.parseResponse = function(response) {
     var uris = [];
 
     $.each(response, function (i, match) {
