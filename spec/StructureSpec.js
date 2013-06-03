61a1e0ca555ebd08fefde06b503d6a18e200a487
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2013-06-03 16:00:32 +0100

    substructure search

--- spec/StructureSpec.js
+++ spec/StructureSpec.js
@@ -62,4 +62,58 @@ describe("Structure search", function() {
       searcher.exact('13413434', 0, null, null, null, callback);
     });
   });
+
+  describe("sub-structure", function() {
+
+    it("can be executed", function() {
+      spyOn(searcher, 'substructure');
+      searcher.substructure('smiles', 'limit', 'start', 'length', 'callback');
+      expect(searcher.substructure).toHaveBeenCalled();
+    });
+    it("and return a response", function() {
+      var this_success = null;
+      var this_status = null;
+      var this_result = null;
+      var callback=function(success, status, response){
+          this_success = success;
+	  this_status = status;
+	  this_result = searcher.parseSubstructureResponse(response);
+      };
+      waitsFor(function() {
+        return this_success != null;
+      });
+      runs(function() {
+        expect(this_success).toEqual(true);
+        expect(this_status).toEqual(200);
+        expect(this_result.length).toBeGreaterThan(1);
+      });
+      searcher.substructure('CNC(=O)c1cc(ccn1)Oc2ccc(cc2)NC(=O)Nc3ccc(c(c3)C(F)(F)F)Cl', null, null, null, callback);
+    });
+    it("executes asynchronously", function() {
+      var callback = jasmine.createSpy();
+      searcher.substructure('CNC(=O)c1cc(ccn1)Oc2ccc(cc2)NC(=O)Nc3ccc(c(c3)C(F)(F)F)Cl', null, null, null, callback);
+      waitsFor(function() {
+          return callback.callCount > 0;
+      });
+      runs(function() {
+          expect(callback).toHaveBeenCalled();
+      });
+    });
+    it("and handle errors", function() {
+      var this_success = null;
+      var this_status = null;
+      var callback=function(success, status){
+          this_success = success;
+	  this_status = status;
+      };
+      waitsFor(function() {
+          return this_status != null;
+      });
+      runs(function() {
+          expect(this_success).toEqual(false);
+          expect(this_status).toEqual(404);
+      });
+      searcher.substructure('13413434', null, null, null, callback);
+    });
+  });
 });
--- src/StructureSearch.js
+++ src/StructureSearch.js
@@ -28,10 +28,37 @@ Openphacts.StructureSearch.prototype.exact = function(smiles, matchType, limit, 
 	});
 }
 
+Openphacts.StructureSearch.prototype.substructure = function(smiles, limit, start, length, callback) {
+        params={};
+        params['_format'] = "json";
+        params['app_key'] = this.appKey;
+        params['app_id'] = this.appID;
+        params['searchOptions.Molecule'] = smiles;
+        limit != null ? params['resultOptions.Limit'] = limit : '';
+        start != null ? params['resultOptions.Start'] = start : '';
+        length != null ? params['resultOptions.Length'] = length : '';
+	var exactQuery = $.ajax({
+		url: this.baseURL + '/structure/substructure',
+                dataType: 'json',
+		cache: true,
+		data: params,
+		success: function(response, status, request) {
+			callback.call(this, true, request.status, response.result.primaryTopic);
+		},
+		error: function(request, status, error) {
+			callback.call(this, false, request.status);
+		}
+	});
+}
+
 Openphacts.StructureSearch.prototype.parseExactResponse = function(response) {
 	return {
                 type: response.type,
                 molecule: response.Molecule,
                 csURI: response.result
         };
+}
+
+Openphacts.StructureSearch.prototype.parseSubstructureResponse = function(response) {
+	return response.result;
 }
