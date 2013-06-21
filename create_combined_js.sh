dd4c2371fa5c91ee3bd3855e71f6be297d276f9f
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2013-06-21 15:42:55 +0100

    script to create the combined js file with all the ops.js code

---
+++ create_combined_js.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm ./src/combined.js
+cat ./src/OPS.js ./src/CompoundSearch.js ./src/ConceptWikiSearch.js ./src/TargetSearch.js ./src/StructureSearch.js ./src/ActivitySearch.js ./src/ChebiSearch.js ./src/EnzymeSearch.js > ./src/combined.js
