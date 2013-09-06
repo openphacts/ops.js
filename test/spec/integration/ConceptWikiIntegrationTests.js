ce0f2f900e9a79d697a16cc384e0fc271a888159
Ian Dunlop <ianwdunlop@gmail.com>
2013-09-06 16:25:28 +0100

    runs, waits for etc for jasmine async tests

--- test/spec/integration/ConceptWikiIntegrationTests.js
+++ test/spec/integration/ConceptWikiIntegrationTests.js
@@ -11,44 +11,111 @@ describe("Concept Wiki", function() {
   describe("search by tag", function() {
 
     it("and return a response for compounds", function() {
+      var this_success = null;
+      var this_status = null;
+      var this_result = null;
+
       var callback=function(success, status, response){
-        var result = searcher.parseResponse(response);
-        expect(result[0]).toBeDefined();
+        this_success = success;
+        this_status = status;
+        this_result = searcher.parseResponse(response);
       };
+
+      waitsFor(function() {
+        return this_result != null;
+      });
+
+      runs(function() {
+        expect(this_result[0]).toBeDefined();
+      });
       searcher.byTag('Aspirin', '20', '4', '07a84994-e464-4bbf-812a-a4b96fa3d197', callback);
     });
     it("and return a response for targets", function() {
+      var this_success = null;
+      var this_status = null;
+      var this_result = null;
+
       var callback=function(success, status, response){
-        var result = searcher.parseResponse(response);
-        expect(result[0]).toBeDefined();
-        expect(result[0].uri).toBeDefined();
-        expect(result[0].prefLabel).toBeDefined();
-        expect(result[0].match).toBeDefined();
+        this_success = success;
+        this_status = status;
+        this_result = searcher.parseResponse(response);
       };
+
+      waitsFor(function() {
+        return this_result != null;
+      });
+
+      runs(function() {
+        expect(this_result[0]).toBeDefined();
+        expect(this_result[0].uri).toBeDefined();
+        expect(this_result[0].prefLabel).toBeDefined();
+        expect(this_result[0].match).toBeDefined();
+      });
       searcher.byTag('Aspirin', '20', '3', 'eeaec894-d856-4106-9fa1-662b1dc6c6f1', callback);
     });
     it("and handle errors", function() {
-      var callback=function(success, status){
-        expect(success).toEqual(false);
-        expect(status).toEqual(500);
+      var this_success = null;
+      var this_status = null;
+      var this_result = null;
+
+      var callback=function(success, status, response){
+        this_success = success;
+        this_status = status;
       };
+
+      waitsFor(function() {
+        return this_status != null;
+      });
+
+      runs(function() {
+        expect(this_success).toEqual(false);
+        expect(this_status).toEqual(500);
+      });
       searcher.byTag('Aspirin', '20', '4', '07a84994-e464-4b96fa3d197', callback);
     });
   });
   describe("search for compounds", function() {
 
     it("and return a response for compounds", function() {
+      var this_success = null;
+      var this_status = null;
+      var this_result = null;
+
       var callback=function(success, status, response){
-        var result = searcher.parseResponse(response);
-        expect(result[0]).toBeDefined();
+        this_success = success;
+        this_status = status;
+        this_result = searcher.parseResponse(response);
       };
+
+      waitsFor(function() {
+        return this_result != null;
+      });
+
+      runs(function() {
+        expect(this_success).toEqual(true);
+        expect(this_status).toEqual(200);
+        expect(this_result[0]).toBeDefined();
+      });
       searcher.findCompounds('Aspirin', '20', '4', callback);
     });
     it("and handle errors", function() {
-      var callback=function(success, status){
-        expect(success).toEqual(false);
-        expect(status).toEqual(500);
+      var this_success = null;
+      var this_status = null;
+      var this_result = null;
+
+      var callback=function(success, status, response){
+        this_success = success;
+        this_status = status;
       };
+
+      waitsFor(function() {
+        return this_status != null;
+      });
+
+      runs(function() {
+        expect(this_success).toEqual(false);
+        expect(this_status).toEqual(500);
+      });
       searcher.findCompounds('Aspirin', '-1', '-1', callback);
     });
  });
@@ -56,46 +123,120 @@ describe("Concept Wiki", function() {
   describe("search for targets", function() {
 
     it("and return a response for targets", function() {
+      var this_success = null;
+      var this_status = null;
+      var this_result = null;
+
       var callback=function(success, status, response){
-        var result = searcher.parseResponse(response);
-        expect(result[0]).toBeDefined();
-        expect(result[0].uri).toBeDefined();
-        expect(result[0].prefLabel).toBeDefined();
-        expect(result[0].match).toBeDefined();
+        this_success = success;
+        this_status = status;
+        this_result = searcher.parseResponse(response);
       };
+
+      waitsFor(function() {
+        return this_result != null;
+      });
+
+      runs(function() {
+        expect(this_success).toEqual(true);
+        expect(this_status).toEqual(200);
+        expect(this_result[0]).toBeDefined();
+        expect(this_result[0].uri).toBeDefined();
+        expect(this_result[0].prefLabel).toBeDefined();
+        expect(this_result[0].match).toBeDefined();
+      });
       searcher.findTargets('PDE5', '20', '3', callback);
     });
     it("and handle errors", function() {
       var callback=function(success, status){
-        expect(success).toEqual(false);
-        expect(status).toEqual(500);
+        expect(this_success).toEqual(false);
+        expect(this_status).toEqual(500);
       };
+      var this_success = null;
+      var this_status = null;
+      var this_result = null;
+
+      var callback=function(success, status, response){
+        this_success = success;
+        this_status = status;
+      };
+
+      waitsFor(function() {
+        return this_status != null;
+      });
+
+      runs(function() {
+        expect(this_success).toEqual(false);
+        expect(this_status).toEqual(500);
+      });
       searcher.findTargets('PDE5', '-1', '-1', callback);
     });
-    it("and handle an empty response for targets", function() {
+    it("and handle an empty response for compounds", function() {
+      var this_success = null;
+      var this_status = null;
+      var this_result = null;
+
       var callback=function(success, status, response){
-        var result = searcher.parseResponse(response);
-        expect(result[0]).toBeDefined();
+        this_success = success;
+        this_status = status;
+        this_result = searcher.parseResponse(response);
       };
+
+      waitsFor(function() {
+        return this_result != null;
+      });
+
+      runs(function() {
+        expect(this_success).toEqual(true);
+        expect(this_status).toEqual(200);
+      });
       searcher.findCompounds('Sorafenib', '20', '4', callback);
     });
  });
   describe("find a single concept", function() {
 
     it("and return a response", function() {
+      var this_success = null;
+      var this_status = null;
+      var this_result = null;
+
       var callback=function(success, status, response){
-        var result = searcher.parseFindConceptResponse(response);
-        expect(result.altLabels).toBeDefined();
-        expect(result.prefLabel).toBeDefined();
-        expect(result.definition).toBeDefined();
+        this_success = success;
+        this_status = status;
+        this_result = searcher.parseFindConceptResponse(response);
       };
+
+      waitsFor(function() {
+        return this_result != null;
+      });
+
+      runs(function() {
+        expect(this_success).toEqual(true);
+        expect(this_status).toEqual(200);
+        expect(this_result.altLabels).toBeDefined();
+        expect(this_result.prefLabel).toBeDefined();
+        expect(this_result.definition).toBeDefined();
+      });
       searcher.findConcept('8e3a87ae-345d-4c25-bd7a-5b3221c6e3fa', callback);
     });
     it("and handle errors", function() {
-      var callback=function(success, status){
-        expect(success).toEqual(false);
-        expect(status).toEqual(500);
+      var this_success = null;
+      var this_status = null;
+      var this_result = null;
+
+      var callback=function(success, status, response){
+        this_success = success;
+        this_status = status;
       };
+
+      waitsFor(function() {
+        return this_status != null;
+      });
+
+      runs(function() {
+        expect(this_success).toEqual(false);
+        expect(this_status).toEqual(500);
+      });
       searcher.findConcept('07a84994-e464-4b96fa3d197', callback);
     });
   });
