4c2a79c96a1e3da19785016845e6ebff8832e2e4
Ian Dunlop <ianwdunlop@gmail.com>
2013-10-14 12:25:02 +0100

    fetch compounds has a lens param

--- test/spec/CompoundSpec.js
+++ test/spec/CompoundSpec.js
@@ -12,12 +12,12 @@ describe("Compound search", function() {
 
     it("can be executed", function() {
       spyOn(searcher, 'fetchCompound');
-      searcher.fetchCompound('compoundURI', 'callback');
+      searcher.fetchCompound('compoundURI', 'lens', 'callback');
       expect(searcher.fetchCompound).toHaveBeenCalled();
     });
     it("executes asynchronously", function() {
       var callback = jasmine.createSpy();
-      searcher.fetchCompound('http://www.conceptwiki.org/concept/38932552-111f-4a4e-a46a-4ed1d7bdf9d5', callback);
+      searcher.fetchCompound('http://www.conceptwiki.org/concept/38932552-111f-4a4e-a46a-4ed1d7bdf9d5', null, callback);
       waitsFor(function() {
           return callback.callCount > 0;
       });
