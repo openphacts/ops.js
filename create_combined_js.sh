b840b3597844122e24a7e9ba1fae5d5adbc56ce8
Ian Dunlop <ianwdunlop@gmail.com>
2013-11-11 16:19:47 +0000

    add data sources to the combined file

--- create_combined_js.sh
+++ create_combined_js.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm ./src/combined.js
-cat ./src/OPS.js ./src/Constants.js ./src/CompoundSearch.js ./src/ConceptWikiSearch.js ./src/TargetSearch.js ./src/StructureSearch.js ./src/ActivitySearch.js ./src/TreeSearch.js ./src/PathwaySearch.js ./src/MapSearch.js > ./src/combined.js
+cat ./src/OPS.js ./src/Constants.js ./src/CompoundSearch.js ./src/ConceptWikiSearch.js ./src/TargetSearch.js ./src/StructureSearch.js ./src/ActivitySearch.js ./src/TreeSearch.js ./src/PathwaySearch.js ./src/MapSearch.js ./src/DataSources.js > ./src/combined.js
--- src/combined.js
+++ src/combined.js
@@ -2172,3 +2172,28 @@ Openphacts.MapSearch.prototype.parseMapURLResponse = function(response) {
         }
 	return urls;
 }
+Openphacts.DataSources = function DataSources(baseURL, appID, appKey) {
+        this.baseURL = baseURL;
+        this.appID = appID;
+        this.appKey = appKey;
+}
+
+Openphacts.DataSources.prototype.getSources = function(callback) {
+        var sourcesQuery = $.ajax({
+                url: this.baseURL + '/sources',
+                dataType: 'json',
+                cache: true,
+                data: {
+                        _format: "json",
+                        app_id: this.appID,
+                        app_key: this.appKey
+                },
+                success: function(response, status, request) {
+                        callback.call(this, true, request.status, response.result.primaryTopic);
+                },
+                error: function(request, status, error) {
+                        callback.call(this, false, request.status);
+                }
+        });
+}
+
