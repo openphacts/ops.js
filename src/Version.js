0631a6ddfd2797a93c6100429a8f6f0e84171b45
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-09-29 13:21:53 +0100

    version number changes

--- src/Version.js
+++ src/Version.js
@@ -4,16 +4,16 @@ Openphacts.Version = function Version() {
 
 Openphacts.Version.prototype.information = function() {
 	return {
-               "version": "2.0.3", 
+               "version": "3.0", 
                "author": "Ian Dunlop",
                "title": "OPS.js",
                "description": "Javascript library for accessing the Open PHACTS Linked Data API",
                "project": "Open PHACTS",
                "organization": "School of Computer Science",
                "address": "University of Manchester, UK",
-               "year": "2013",
-               "month": "December",
+               "year": "2014",
+               "month": "September",
                "url": "http://github.com/openphacts/ops.js",
-               "LDA-version": "1.3"
+               "LDA-version": "1.4"
            }; 
 };
