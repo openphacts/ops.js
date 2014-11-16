4b4698c760aa7fb4c401cca79081f3b27b233bbb
Egon Willighagen <egon.willighagen@gmail.com>
2014-11-16 16:36:12 +0100

    Added documentation to DataSources.getSources

--- src/DataSources.js
+++ src/DataSources.js
@@ -13,6 +13,21 @@ Openphacts.DataSources = function DataSources(baseURL, appID, appKey) {
         this.appKey = appKey;
 }
 
+/**
+ * Fetch a list of data sources used in the Open PHACTS linked data cache.
+ *
+ * @param {requestCallback} callback - Function that will be called with success, status, and JSON response values.
+ * @method
+ * @example
+ * var datasources = new Openphacts.DataSources("https://beta.openphacts.org/1.4", appID, appKey);
+ * var callback = function(success, status, response) {
+ *    var subsets = response.primaryTopic.subset;
+ *    for (i=0; subsets.length; i++) {
+ *      console.log("Subset: " + subsets[i].title);
+ *    }
+ * };
+ * datasources.getSources(callback);
+ */
 Openphacts.DataSources.prototype.getSources = function(callback) {
         var sourcesQuery = $.ajax({
                 url: this.baseURL + '/sources',
