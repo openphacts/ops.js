061908a43be5db44dd592dbbb1f06b58deff7a27
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2015-05-12 14:01:17 +0100

    target organism name in target pharma

--- src/TargetSearch.js
+++ src/TargetSearch.js
@@ -627,7 +627,7 @@ Openphacts.TargetSearch.prototype.parseTargetPharmacologyResponse = function(res
                 target_title = target.title;
 		target_uri = target._about;
                 target_provenance = 'https://www.ebi.ac.uk/chembl/target/inspect/' + target._about.split('/').pop();
-		target_organism_name = target.assay_organism != null ? target.assay_organism : null;
+		target_organism_name = target.targetOrganismName != null ? target.targetOrganismName : null;
 		if (target.hasTargetComponent != null) {
 			Openphacts.arrayify(target.hasTargetComponent).forEach(function(targetComponent, i) {
 				var tc = {};
@@ -666,7 +666,6 @@ Openphacts.TargetSearch.prototype.parseTargetPharmacologyResponse = function(res
             'compoundInchikey': compound_inchikey,
             //compoundDrugType: compound_drug_type,
             //compoundGenericName: compound_generic_name,
-            'targetTitle': target_title,
             //targetConcatenatedUris: target_concatenated_uris,
 
             'compoundInchikeySrc': cs_src,
