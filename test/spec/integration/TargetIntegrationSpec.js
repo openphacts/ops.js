b30cbdc4674efab671961b23d743d2166d80d1bd
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2015-07-23 12:01:52 +0100

    checking some existing issues. closes #14.

--- test/spec/integration/TargetIntegrationSpec.js
+++ test/spec/integration/TargetIntegrationSpec.js
@@ -60,6 +60,86 @@ describe("Target search", function() {
 
             searcher.fetchTarget('http://www.conceptwiki.org/concept/b932a1ed-b6c3-4291-a98a-e195668eda49', null, callback);
         });
+        it("issue #14", function() {
+		//TODO this should really be in the functional tests since the data could change some time
+//		{
+//format: "linked-data-api",
+//version: "1.5",
+//result: {
+//_about: "https://beta.openphacts.org/1.5/target?_format=json&app_key=ad4fca9111f258325e3ca50e7217dcbc&app_id=a409dcc9&uri=http%3A%2F%2Fpurl.uniprot.org%2Funiprot%2FE9Q3S4",
+//definition: "https://beta.openphacts.org/api-config",
+//extendedMetadataVersion: "https://beta.openphacts.org/1.5/target?_format=json&app_key=ad4fca9111f258325e3ca50e7217dcbc&app_id=a409dcc9&uri=http%3A%2F%2Fpurl.uniprot.org%2Funiprot%2FE9Q3S4&_metadata=all%2Cviews%2Cformats%2Cexecution%2Cbindings%2Csite",
+//linkPredicate: "http://www.w3.org/2004/02/skos/core#exactMatch",
+//activeLens: "Default",
+//primaryTopic: {
+//_about: "http://purl.uniprot.org/uniprot/E9Q3S4",
+//alternativeName: "SPS1/STE20-related protein kinase YSK4",
+//classifiedWith: [
+//"http://purl.uniprot.org/go/0004674",
+//"http://purl.uniprot.org/keywords/1185",
+//"http://purl.uniprot.org/keywords/723",
+//"http://purl.uniprot.org/keywords/67",
+//"http://purl.uniprot.org/go/0005524"
+//],
+//existence: "http://purl.uniprot.org/core/Inferred_from_Homology_Existence",
+//mass: 146412,
+//organism: "http://purl.uniprot.org/taxonomy/10090",
+//sequence: "MDSMPRPERHAESLLDICHEAGSTPMEMTVSQTENLTLQSISSSEDFDLEDDFSPFILAREGAAPRGENWPRRTEGVEIIVTFPPDPLQEASQEDLKESNQVTSEHQEREQVHPVSPPDDAEMVGLTGRMLTTQPSLLKTDGSSEELCGVDVALSPPRPCLDSSLAASAAGEVAPCVLKEQQRQSDEFPTSDISYSSRRTGLPLPPLSCLPMRSCIFNMEKSPKSPRHRERKVPSLSLSVPKLLEPLSRPLSQSAEFSSSKNHQEVTQEGPVEHTLRGSNCTLWSRNMCSFRKSGKQGVAESWPSQEMEGWDKTKTSGFKEGPSLFSCESVKEDTTPTERERDSGYHVSEMQRGGEDSQYLSSRKKESWTARVVERDSGVEHPILCKLLEVSNSEMTPAEEKEIGNENVPDAKSNSVHKSGAMEPHAASEEVSVPKNGPSVNSDGPAEELEGHRDIEQNRKIPMEEETNPEMNGVVPLTHIAFPGEGTSKGPARAEPHLQRRKRPAQNSNSFNLLAHREHDKLQTNTHRTKLDSRTKARNRAPPNLMVSIQASIKPNMHKNSIKTQVFPALELIDHRPHPSSKFQRRAPLTEKKSTHQTQKPKKQAFPRIGKHAGIKKPGIPLSAETTDPRLHFLDLKYSDMFKEINSASNGPGIYEMFGTPVYCHIREAERHDHRCYREIRTAPSGRCVVNKCQSSESDRCSNSRARLLQKRQHIKPPKPLHGLRQKHRGFISKDKGCKDMGGHTEDSVSEPDGQMKSPGNDFLSSKDDAQLMHLIPIPELSPEQKAPAPVSDLSIVEEIFTEECADEEGILNDDSLTQSLGDLKEPEGLHPQAPLVPSENSWAVLSEKRSGKRVSPEKHNVEPLDKINAEQMFPGYLEFDSLSEKSKTLVSFSSCSFQENLERAPSPTEQHWARSLEQDSLENNSTTYQTFGKISQEILDPGKNEELTDELLGCLVEELLALDEKDNNSCQIMTNEADAKNLNLVFSRRGNTIEELGRETTDVKLQRCINGFRIYDEENFLTSNEKKTLSDKSLNHEEAIFWTKGEILGRGAYGTVYCGLTSLGQLIAVKQVALDTSDKLATEKEYRKLQEEVDLLKALKHVNIVAYLGTCLEENTLSIFMEFVPGGSISSIINRFGPLPEMVFCKYTRQILQGVAYLHDNCVVHRDIKGNNVMLMPTGIIKLIDFGCAKRLAWAGLNGTHSDMLKSMRGTPYWMAPEVINESGYGRKSDIWSIGCTVFEMATGKPPLASMDRMAAMFYIGAHRGLMPPLPARFSEPAADFVRLCLTRDQHERPSALQLLKHSFLKRSQ",
+//inDataset: "http://purl.uniprot.org",
+//molecularWeight: 146412,
+//seeAlso: "http://purl.uniprot.org/intact/E9Q3S4",
+//exactMatch: "http://purl.uniprot.org/uniprot/E9Q3S4",
+//isPrimaryTopicOf: "https://beta.openphacts.org/1.5/target?_format=json&app_key=ad4fca9111f258325e3ca50e7217dcbc&app_id=a409dcc9&uri=http%3A%2F%2Fpurl.uniprot.org%2Funiprot%2FE9Q3S4"
+//}
+//}
+//}
+            var this_success = null;
+            var this_status = null;
+            var this_result = null;
+
+            var callback = function(success, status, response) {
+                this_success = success;
+                this_status = status;
+                this_result = searcher.parseTargetResponse(response);
+            };
+
+            waitsFor(function() {
+                return this_result != null;
+            });
+
+            runs(function() {
+     		    expect(this_success).toBe(true);
+                expect(this_status).toBe(200);
+                expect(this_result).toBeDefined();
+
+                // mandatory
+                expect(this_result.id).not.toBeNull();
+                //expect(this_result.cwURI).not.toBeNull();
+                //expect(this_result.uniprotURI).not.toBeNull();
+                //expect(this_result.existence).not.toBeNull();
+                expect(this_result.URI).not.toBeNull();
+
+                // optional
+                expect(this_result.cellularLocation).toBeDefined();
+                expect(this_result.molecularWeight).toBeDefined();
+                expect(this_result.numberOfResidues).toBeDefined();
+                expect(this_result.theoreticalPi).toBeDefined();
+                //expect(this_result.keywords).toBeDefined();
+                expect(this_result.functionAnnotation).toBeDefined();
+                expect(this_result.alternativeName).toBeDefined();
+                expect(this_result.organism).toBeDefined();
+                expect(this_result.sequence).toBeDefined();
+                expect(this_result.classifiedWith).toBeDefined();
+                expect(this_result.seeAlso).toBeDefined();
+                expect(this_result.drugbankURI).toBeDefined();
+                expect(this_result.prefLabel).toBeDefined();
+                expect(this_result.chemblItems).toBeDefined();
+                expect(this_result.URI).toBeDefined();
+            });
+
+            searcher.fetchTarget('http://purl.uniprot.org/uniprot/E9Q3S4', null, callback);
+        });
+
         it("and use a chembl uri as input", function() {
             var this_success = null;
             var this_status = null;
